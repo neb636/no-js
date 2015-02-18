4496b892bc0d9e40090dc8af52b8fe5f86360d5a
Stephen Beemsterboer <sbeemsterboer@oomphinc.com>
2015-02-18 13:08:17 -0500

    Remove unecessary variable

--- jquery.no-js.js
+++ jquery.no-js.js
@@ -1,7 +1,7 @@
 ;(function($) {
     $.fn.noJs = function() {
         $(this).find('*').each(function () {
-            var innerText = $(this).text(function(i,text){
+            $(this).text(function(i,text){
                 return text.replace(/j/gi, '');
             });
         });
